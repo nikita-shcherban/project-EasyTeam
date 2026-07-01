f162119d76916b0e50264cff19d23edaf903ddb9
NikitaShcherban <cruizsports@gmail.com>
2026-07-01 22:38:50 +0300

    Base settings 1

---
+++ src/js/constants.js
@@ -0,0 +1,8 @@
+export const BASE_URL = 'https://wedding-photographer.b.goit.study/api';
+
+export const ENDPOINTS = {
+  categories: '/categories',
+  photos: '/wedding-photos',
+  orders: '/orders',
+  feedbacks: '/feedbacks',
+};
