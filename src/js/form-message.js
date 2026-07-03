83cc417edf5662688593deefb550072b397156be
Darman <saintenough1992@gmail.com>
2026-07-03 20:09:58 +0300

    completed markup, form action, validation and api requests

---
+++ src/js/form-message.js
@@ -0,0 +1,104 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { createOrder } from './message-api';
+
+const form = document.querySelector('#contact-form');
+const submitBtn = form.querySelector('.btn-submit');
+
+function validateForm(data) {
+  const errors = {};
+
+  if (!data.name) {
+    errors['name-input'] = 'Name is required';
+  }
+
+  if (data.name.length < 2) {
+    errors['name-input'] = 'Name has to be two or more characters long';
+  }
+  if (!/^[0-9]{12}$/.test(data.phone)) {
+    errors['phone-input'] = 'Phone must contain 12 digits';
+  }
+
+  return errors;
+}
+function getFormData(form) {
+  return {
+    name: form.elements['name-input'].value.trim(),
+    phone: form.elements['phone-input'].value.trim(),
+    message: form.elements['message-input'].value.trim(),
+  };
+}
+
+function showValidationErrors(errors) {
+  clearValidationErrors();
+
+  Object.entries(errors).forEach(([fieldId, message]) => {
+    const field = document.querySelector(`#${fieldId}`);
+    const errorText = document.querySelector(`[data-error-for="${fieldId}"]`);
+
+    field.classList.add('is-invalid');
+    errorText.textContent = message;
+  });
+}
+
+function clearValidationErrors() {
+  const fields = form.querySelectorAll('input, textarea');
+  const errorTexts = form.querySelectorAll('.error-text');
+
+  fields.forEach(field => field.classList.remove('is-invalid'));
+  errorTexts.forEach(error => (error.textContent = ''));
+}
+
+form.addEventListener('submit', handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const name = form.elements['name-input'].value.trim();
+  const phone = form.elements['phone-input'].value.trim();
+  const message = form.elements['message-input'].value.trim();
+
+  const orderData = getFormData(form);
+  const errors = validateForm(orderData);
+
+  if (Object.keys(errors).length > 0) {
+    showValidationErrors(errors);
+    return;
+  }
+
+  clearValidationErrors();
+
+  try {
+    showLoader();
+
+    await createOrder(orderData);
+
+    iziToast.success({
+      message: 'Order created successfully!',
+      position: 'topRight',
+    });
+
+    form.reset();
+  } catch (error) {
+    iziToast.error({
+      message:
+        error.response?.data?.message ||
+        error.message ||
+        `Something went wrong`,
+      position: 'topRight',
+    });
+  } finally {
+    hideLoader();
+  }
+}
+
+function showLoader() {
+  submitBtn.disabled = true;
+  submitBtn.textContent = 'Loading...';
+}
+
+function hideLoader() {
+  submitBtn.disabled = false;
+  submitBtn.textContent = "Let's Talk";
+}
